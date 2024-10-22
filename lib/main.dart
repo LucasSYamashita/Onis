9f91c2dc814d4797f5e001bb3d7b84b0f5ca4787
LucasSYamashita <lucasyamashita@gmail.com>
2024-10-22 16:17:19 -0400

    Descrição do commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//package:shared_preferences/shared_preferences.dart
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onis',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        color: Colors.white , 
+        child: Container(
+          color: Colors.blue,),),
+    );
+  
+  }
+}
