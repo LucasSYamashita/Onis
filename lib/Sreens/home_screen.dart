3361e5d104bc1ded13b47bf6e04c76f95481025d
Lucas Yamashita <98978376+LucasSYamashita@users.noreply.github.com>
2024-10-23 15:53:17 -0400

    Update home_screen.dart

--- lib/Sreens/home_screen.dart
+++ lib/Sreens/home_screen.dart
@@ -5,10 +5,117 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bem-vindo!'),
+        title: const Text('Usuario nome'),
       ),
-      body: Center(
-        child: Text('Você está logado!', style: TextStyle(fontSize: 24)),
+      body: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Entradas') 
+                      
+                    ),
+                    ),
+                  Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Pratos Frios') 
+                      
+                    ),
+                    ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Pratos Quentes') 
+                      
+                    ),
+                    ),
+                  Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Porções/Extras ') 
+                      
+                    ),
+                    ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Sobremesas') 
+                      
+                    ),
+                    ),
+                 Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Bebidas') 
+                      
+                    ),
+                    ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 250,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: (){print('Botao');},
+                      child: const Text('Chamar atendente') 
+                      
+                    ),
+                    ),
+                ],
+              )
+            ],
+          ),
       ),
     );
   }
